034146f6e6ef2ce7839a19817c9492c821256360
Yusuf Ömer Sözdutmaz <72439241+daeSundae@users.noreply.github.com>
2021-04-23 11:15:27 +0300

    RMX2001: fix padding
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>
    Change-Id: Ief8abc11b25e4098ba514c38ca6ae4887fe51af5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -140,6 +140,8 @@ TW_OZIP_DECRYPT_KEY := 0000
 TW_INCLUDE_RESETPROP := true
 TW_INCLUDE_REPACKTOOLS := true
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone3/temp
+TW_Y_OFFSET := 115
+TW_H_OFFSET := -115
 
 # Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
